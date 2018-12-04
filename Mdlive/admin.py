fea01f40c6ce81b0ff6bd35c080f301f637e9f55
sachinprajapati <shivangi.srivastava@rootinfosolutions.net>
2018-12-04 20:34:37 +0530

    done

--- Mdlive/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-admin.site.register(MdliveRegistration)
-admin.site.register(MdliveUserToken)
-admin.site.register(MdliveAppAuth)
--- SalesForce/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url,include
-from .views import *
-urlpatterns = [
-
-	# url(r'^redirecturl/', Redirecturlsalesforce.as_view()),
-	url(r'^signupstep/', SearchContactSalesForce.as_view(), name='step1'),
-    # url(r'^getcode/', GetCode.as_view()),
-    
-]
